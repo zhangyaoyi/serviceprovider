47a5ada7882ec4606f8ae35cb508dd5d85062b38
leo.zhang <yzhang252@dxc.com>
2021-01-13 16:10:13 +0800

    Initial commit

---
+++ src/main/java/com/imooc/springcloud/ServiceProviderApplication.java
@@ -0,0 +1,9 @@
+package com.imooc.springcloud;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ServiceProviderApplication {
+}
