a731aad6983b27eb06179a5826747989ffb1edd0
leo.zhang <yzhang252@dxc.com>
2021-01-13 17:20:32 +0800

    add

---
+++ src/main/java/com/imooc/springcloud/controller/TestController.java
@@ -0,0 +1,17 @@
+package com.imooc.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @Value("${name}")
+    private String name;
+
+    @GetMapping("name")
+    public String getName(){
+        return name;
+    }
+}
